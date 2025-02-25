4b16355c9d32a1087461a1c8ffb96749d77c46dc
Carlos Díaz <dg.carlosr@gmail.com>
2025-02-24 20:05:13 -0600

    feat: functional auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './components/auth/auth.component';
 
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -7,9 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AuthComponent {
   mostrarLogin: boolean = true;
+  usuario: string = '';
+  password: string = '';
+  usuarioLogueado: string | null = null;  // Variable para almacenar el usuario logueado
 
   toggleFormulario() {
     this.mostrarLogin = !this.mostrarLogin;
   }
 
+  login() {
+    const adminUser = { usuario: 'admin', password: '1234' };
+
+    if (this.usuario === adminUser.usuario && this.password === adminUser.password) {
+      this.usuarioLogueado = this.usuario;  // Guardamos el usuario logueado
+    } else {
+      alert('Usuario o contraseña incorrectos');
+    }
+  }
+
+  logout() {
+    this.usuarioLogueado = null; // Cerramos sesión
+    this.usuario = '';  // Limpiamos campos
+    this.password = '';
+  }
 }
